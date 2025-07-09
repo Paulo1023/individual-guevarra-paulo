d990db7097417a7a171e96e4a4be94af85321d41
pauloguevarra10 <pauloguevarra10@gmail.com>
2025-07-09 11:28:33 +0800

    add s03 discussion and activity

---
+++ s03/discussion/NexusScm/Nexus.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nexus.Core;
+
+namespace Nexus.Web.Data
+{
+    public class ApplicationDbContext : DbContext //allows connection between app and database
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Supplier> Suppliers { get; set; } //access the Supplier table
+    }
+}
