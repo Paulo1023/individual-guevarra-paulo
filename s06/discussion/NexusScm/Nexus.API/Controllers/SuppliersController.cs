52e0056b7de339fd6bb4f3bf19c06dcdd3687ac1
pauloguevarra10 <pauloguevarra10@gmail.com>
2025-07-14 12:06:02 +0800

    Add activity code s06

---
+++ s06/discussion/NexusScm/Nexus.API/Controllers/SuppliersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nexus.API.Data;
+using Nexus.Core;
+
+namespace Nexus.API.Controllers
+{
+    [Route("api/[controller]")] //set route
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; //store context object from ApplicationDbContext this will give access to the tables
+
+        // sets the _context value
+        public SuppliersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            //throw new Exception("This is a deliberate test exception!");
+
+            //list all
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // adds id in the url
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            //needs paramater for find async 
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(supplier); // ok returns Http status code 200 with the supplier 
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                return BadRequest("Supplier cannot be null.");
+            }
+
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSupplier), new { id = supplier.Id }, supplier);
+        }
+    }
+}
---
+++ s06/discussion/NexusScm/Nexus.Core/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nexus.Core
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        //Foreign key
+        [Required]
+        public int SupplierId { get; set; }
+
+        //Navigate property
+        public Supplier Supplier { get; set; }
+    }
+}
