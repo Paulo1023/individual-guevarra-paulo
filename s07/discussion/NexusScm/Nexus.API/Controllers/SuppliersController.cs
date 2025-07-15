99de41271748ea50f5b10e1bdae301d1991229da
pauloguevarra10 <pauloguevarra10@gmail.com>
2025-07-15 12:19:54 +0800

    Add s07 discussion

---
+++ s07/discussion/NexusScm/Nexus.API/Controllers/SuppliersController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client.Kerberos;
+using Nexus.API.Data;
+using Nexus.Core;
+
+namespace Nexus.API.Controllers
+{
+    [Route("api/[controller]")] //set route
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context; //store context object from ApplicationDbContext this will give access to the tables
+
+        // sets the _context value
+        public SuppliersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            //throw new Exception("This is a deliberate test exception!");
+
+            //list all
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        [HttpGet("{id}")] // adds id in the url
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            //needs paramater for find async 
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(supplier); // ok returns Http status code 200 with the supplier 
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            if (supplier == null)
+            {
+                return BadRequest("Supplier cannot be null.");
+            }
+
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSupplier), new { id = supplier.Id }, supplier);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<ActionResult<Supplier>> PutSupplier(int id, Supplier supplier)
+        {
+
+            var existingSupplier = await _context.Suppliers.FindAsync(id);
+            if (existingSupplier == null)
+            {
+                return NotFound("Supplier not found");
+            }
+
+            // update columns
+            existingSupplier.Name = supplier.Name;
+            existingSupplier.ContactPerson = supplier.ContactPerson;
+            existingSupplier.Email = supplier.Email;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Suppliers.Any(s => s.Id == id))
+                {
+                    return NotFound("Supplier no longer exists.");
+                }
+                else
+                {
+                    throw;
+                }
+                
+            }
+            return Ok("Update Successful!");
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<ActionResult<Supplier>> DeleteSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound("Supplier not found.");
+            }
+
+            _context.Suppliers.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return Ok("Deletion Successful!");
+        }
+    }
+}
---
+++ s07/discussion/NexusScm/Nexus.API/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Nexus.API.Middleware
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next; //determine if will go to the next middleware or stop process
+        private readonly ILogger<GlobalExceptionHandler> _logger; // logs the messages
+
+        // constructor to set the _next and _logger
+        public GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        // if no exception the process continue, else produce the custom error
+        public async Task InvokeAsync(HttpContext context) 
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                // for dev error 
+                _logger.LogError(ex, "An unhandled exception has occured");
+                // for user error
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            // set status code to internal server error 500 then custom message for the user
+            var response = new 
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error from the custom handler. Please try again later."
+            };
+            // return the error status and message in json format
+            return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+
+
+    }
+}
